7786cdd4bd03b99ec9cb0b89844384fa69d3ebaf
David Cornu <me@davidcornu.com>
2023-12-04 22:47:18 -0500

    Build binary and container with nix

--- flake.nix
+++ flake.nix
@@ -21,15 +21,85 @@
     , rust-overlay
     }: flake-utils.lib.eachDefaultSystem (
       system:
-      let 
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+
         rustToolchain = pkgs.rust-bin.beta.latest.default.override {
           extensions = [ "rust-src" ];
         };
-      in
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        assetsFilter = path: _type: (builtins.match "^.*/assets/yells_at\.png$" path) != null;
+        filter = path: type: (craneLib.filterCargoSources path type) || (assetsFilter path type);
+
+        src = pkgs.lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          inherit filter;
+        };
+
+        systemBuildInputs =
+          if pkgs.stdenv.isDarwin
+          then
+            [
+              pkgs.darwin.apple_sdk.frameworks.Security
+              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            ]
+          else
+            [ ];
+      in rec
       {
+        packages.default = craneLib.buildPackage {
+          inherit src;
+          strictDeps = true;
+          doCheck = false; # No tests yet
+          nativeBuildInputs = systemBuildInputs;
+
+          RUST_BACKTRACE = 1;
+        };
+
+        packages.procfile = pkgs.writeTextFile {
+          name = "Procfile";
+          text = ''
+            web: ${packages.default}/bin/yells-at
+            nginx: ${pkgs.nginx}/bin/nginx -c ${./config/nginx.conf}
+          '';
+        };
+
+        packages.container = pkgs.dockerTools.buildLayeredImage {
+          name = "yells-at";
+          tag = "latest";
+
+          contents = [ 
+            pkgs.cacert 
+            pkgs.hivemind
+          ];
+
+          config.Cmd = [
+            "hivemind"
+            "${packages.procfile}"
+          ];
+
+          fakeRootCommands = ''
+            #!${pkgs.runtimeShell}
+            set -ex
+            ${pkgs.dockerTools.shadowSetup}
+            groupadd -r nginx
+            useradd -r -g  nginx nginx
+            mkdir -p /tmp/nginx_cache
+          '';
+          enableFakechroot = true;
+        };
+
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ rustToolchain ];
-          buildInputs = with pkgs; [ ];
+          nativeBuildInputs = [ rustToolchain ] ++ systemBuildInputs;
+          buildInputs = with pkgs; [ hivemind nginx ];
         };
       }
     );
